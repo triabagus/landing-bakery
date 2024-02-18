e00d4904550c8cf7edf16eb86f8c84f8ace7f979
triabagus <triatop9@gmail.com>
2024-02-18 20:11:01 +0700

    [Feature] Scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,13 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up')
+  // When the scroll is higher than 350 viewport height, add the scroll class
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                      : scrollUp.classList.remove('show-scroll') 
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
